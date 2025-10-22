b264e6096c11b8097f17f9fedd7c1b06220c1446
ram <rramkumar1306@gmail.com>
2025-10-22 10:45:16 +0530

    chore: use input actions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,20 @@
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     public CharacterController characterController;
     public Animator animator;
+    public InputActionAsset playerActions;
 
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 2f;
 
     [Header("Input")]
-    private float moveInput;
-    private float turnInput;
+    private Vector2 moveInput;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,29 +22,47 @@ public class PlayerMovement : MonoBehaviour
         characterController = GetComponent<CharacterController>();
     }
 
+    void OnEnable()
+    {
+        playerActions.Enable();
+        playerActions["move"].performed += OnMove;
+        playerActions["move"].canceled += OnMove;
+    }
+
+    void OnDisable()
+    {
+        playerActions.Disable();
+        playerActions["move"].performed -= OnMove;
+        playerActions["move"].canceled -= OnMove;
+    }
     // Update is called once per frame
     void Update()
     {
-        InputManagement();
+        //InputManagement();
         Movement();
         HandleAnimation();
     }
 
     private void Movement()
     {
-        Vector3 move = new Vector3(moveInput, 0f, turnInput);
+        Vector3 move = new(moveInput.x, 0f, moveInput.y);
         move *= moveSpeed;
         characterController.Move(move * Time.deltaTime);
     }
 
-    private void InputManagement()
-    {
-        moveInput = Input.GetAxis("Horizontal");
-        turnInput = Input.GetAxis("Vertical");
-    }
+    //private void InputManagement()
+    //{
+    //    moveInput = Input.GetAxis("Horizontal");
+    //    turnInput = Input.GetAxis("Vertical");
+    //}
 
     private void HandleAnimation()
     {
-        animator.SetBool("walking", characterController.velocity != Vector3.zero);
+        animator.SetBool("Walking", characterController.velocity != Vector3.zero);
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
