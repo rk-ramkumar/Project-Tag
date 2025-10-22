a1dcadc831ad82a236166cd1d4cf498fd0c74ef0
ram <rramkumar1306@gmail.com>
2025-10-22 10:03:40 +0530

    feat: implement walk animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     public CharacterController characterController;
+    public Animator animator;
 
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float moveSpeed = 2f;
 
     [Header("Input")]
     private float moveInput;
@@ -23,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
     {
         InputManagement();
         Movement();
+        HandleAnimation();
     }
 
     private void Movement()
@@ -30,12 +32,16 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = new Vector3(moveInput, 0f, turnInput);
         move *= moveSpeed;
         characterController.Move(move * Time.deltaTime);
-
     }
 
     private void InputManagement()
     {
         moveInput = Input.GetAxis("Horizontal");
         turnInput = Input.GetAxis("Vertical");
+    }
+
+    private void HandleAnimation()
+    {
+        animator.SetBool("walking", characterController.velocity != Vector3.zero);
     }
 }
