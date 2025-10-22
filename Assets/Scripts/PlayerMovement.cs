d4b332d4b6fd8fa6366f2ef90c7834ae7cb659ca
ram <rramkumar1306@gmail.com>
2025-10-22 14:08:37 +0530

    refactor

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,67 +1,150 @@
-using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
-    public CharacterController characterController;
+    public CharacterController controller;
     public Animator animator;
     public InputActionAsset playerActions;
+    public Transform cameraPivot;
+    public CinemachineCamera cinemachineCam;
 
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float sprintSpeed = 8f;
+    [SerializeField] private float acceleration = 6f;
+    [SerializeField] private float rotationSpeed = 12f;
+    [SerializeField] private float gravity = -9.81f;
 
-    [Header("Input")]
+    private float currentSpeed;
+    private float targetSpeed;
+    private Vector3 velocity;
+    private bool _isSprinting;
     private Vector2 moveInput;
-   
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Camera Settings")]
+    [SerializeField] private float cameraSensitivity = 120f;
+    [SerializeField] private float minPitch = -30f;
+    [SerializeField] private float maxPitch = 60f;
+    private float yaw;
+    private float pitch;
+
+    private InputAction moveAction;
+    private InputAction sprintAction;
+    private InputAction lookAction;
+    private int velocityHash;
+
+    public bool IsSprinting
+    {
+        get => _isSprinting;
+        set
+        {
+            _isSprinting = value;
+            targetSpeed = value ? sprintSpeed : walkSpeed;
+        }
+    }
+
+    void Awake()
+    {
+        moveAction = playerActions.FindAction("move");
+        sprintAction = playerActions.FindAction("Sprint");
+        lookAction = playerActions.FindAction("Look");
+    }
+
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        velocityHash = Animator.StringToHash("Velocity");
+        targetSpeed = walkSpeed;
+
+        // Initialize yaw/pitch from current cameraPivot
+        Vector3 angles = cameraPivot.eulerAngles;
+        yaw = angles.y;
+        pitch = angles.x;
     }
 
     void OnEnable()
     {
         playerActions.Enable();
-        playerActions["move"].performed += OnMove;
-        playerActions["move"].canceled += OnMove;
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
     }
 
     void OnDisable()
     {
         playerActions.Disable();
-        playerActions["move"].performed -= OnMove;
-        playerActions["move"].canceled -= OnMove;
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
     }
-    // Update is called once per frame
+
     void Update()
     {
-        //InputManagement();
-        Movement();
+        HandleSprintToggle();
+        HandleCameraLook();
+        HandleMovement();
         HandleAnimation();
     }
 
-    private void Movement()
+    void HandleSprintToggle()
     {
-        Vector3 move = new(moveInput.x, 0f, moveInput.y);
-        move *= moveSpeed;
-        characterController.Move(move * Time.deltaTime);
+        if (sprintAction != null && sprintAction.WasPressedThisFrame())
+            IsSprinting = !IsSprinting;
     }
 
-    //private void InputManagement()
-    //{
-    //    moveInput = Input.GetAxis("Horizontal");
-    //    turnInput = Input.GetAxis("Vertical");
-    //}
-
-    private void HandleAnimation()
+    void HandleCameraLook()
     {
-        animator.SetBool("Walking", characterController.velocity != Vector3.zero);
+        Vector2 lookDelta = lookAction.ReadValue<Vector2>();
+
+        // On mobile, lookAction will be triggered by right-side drag gesture
+        yaw += lookDelta.x * cameraSensitivity * Time.deltaTime;
+        pitch -= lookDelta.y * cameraSensitivity * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        // Apply rotation to the camera pivot
+        cameraPivot.rotation = Quaternion.Euler(pitch, yaw, 0f);
     }
 
-    private void OnMove(InputAction.CallbackContext context)
+    void HandleMovement()
+    {
+        // Smoothly interpolate movement speed
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, acceleration * Time.deltaTime);
+
+        // Read movement vector
+        Vector3 moveDir = new(moveInput.x, 0f, moveInput.y);
+        moveDir = cameraPivot.forward * moveDir.z + cameraPivot.right * moveDir.x;
+        moveDir.y = 0f;
+        moveDir.Normalize();
+
+        // Rotate player towards movement direction
+        if (moveDir.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        // Gravity
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+        velocity.y += gravity * Time.deltaTime;
+
+        // Apply motion
+        Vector3 move = moveDir * currentSpeed + new Vector3(0, velocity.y, 0);
+        controller.Move(move * Time.deltaTime);
+    }
+
+    void HandleAnimation()
+    {
+        Vector3 horizontalVelocity = new(controller.velocity.x, 0, controller.velocity.z);
+        float speed = horizontalVelocity.magnitude;
+        float normalizedSpeed = Mathf.InverseLerp(0f, sprintSpeed, speed);
+
+        animator.SetFloat(velocityHash, normalizedSpeed);
+    }
+
+    void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
